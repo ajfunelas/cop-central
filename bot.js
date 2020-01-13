6e1c4d279b29b451e51b15858637638efcd9dbcc
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-01-13 22:39:06 +0800

    last update

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://www.streetwearofficial.com/collections/supreme/products/supreme-box-logo-sticker');
   await page.click("#product-add-to-cart");
-  await page.waitFor(20);
-  await page.click("#supreme-supreme-box-logo-sticker > div.ajax-success-normal.ajax-success-modal.halo-modal > div > div > div > div.actions > button.btn.btn-go-to-cart");
+//   await page.waitFor(20);
+//   await page.click("#supreme-supreme-box-logo-sticker > div.ajax-success-normal.ajax-success-modal.halo-modal > div > div > div > div.actions > button.btn.btn-go-to-cart");
 
   await browser.close();
 })();
