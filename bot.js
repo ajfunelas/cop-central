0418bab2a235663b310f8f9d5fd8c578ace7c279
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-01-13 22:32:16 +0800

    test click

--- bot.js
+++ bot.js
@@ -3,8 +3,10 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://www.streetwearofficial.com/collections/supreme/products/supreme-box-logo-sticker');
+  await page.click("#product-add-to-cart");
+  await page.waitFor(20);
+  await page.click("#supreme-supreme-box-logo-sticker > div.ajax-success-normal.ajax-success-modal.halo-modal > div > div > div > div.actions > button.btn.btn-go-to-cart");
 
   await browser.close();
 })();
