7e3e88d52a715cceb4e8b748b915b9da9fbd56fc
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-01-16 00:14:41 +0800

    chromium launches!!! :100:
    - added ignoreHTTPSErrors: true,
    - headless: false

--- snkrs.js
+++ snkrs.js
@@ -1,27 +1,27 @@
 const puppeteer = require('puppeteer');
 (async () => {
-  const browser = await puppeteer.launch({ executablePath: 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'})
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch({
+		ignoreHTTPSErrors: true,
+		headless: false
+	});
+  const page = await browser.newPage();
   
-  const navigationPromise = page.waitForNavigation()
+  const navigationPromise = page.waitForNavigation();
+
+  await page.goto('https://www.footlocker.com.au/en/p/puma-rs-x3-men-shoes-90882?v=244205324304#!searchCategory=men');
   
-  await page.goto('https://www.nike.com/au/launch/t/air-vapormax-plus-on-air-paris/')
+  await page.setViewport({ width: 1279, height: 618 });
   
-  await page.setViewport({ width: 1279, height: 618 })
+  await page.waitForSelector('#fitanalytics_sizecontainer > .fl-accordion-tab--content:nth-child(6) > .fl-product-size > .fl-product-size--item:nth-child(5) > span');
+  await page.click('#fitanalytics_sizecontainer > .fl-accordion-tab--content:nth-child(6) > .fl-product-size > .fl-product-size--item:nth-child(5) > span');
   
-  await page.waitForSelector('.ncss-col-sm-12 > .pdp-container > .card-product-component > .ncss-col-sm-12 > .product-info-container')
-  await page.click('.ncss-col-sm-12 > .pdp-container > .card-product-component > .ncss-col-sm-12 > .product-info-container')
+  await page.waitForSelector('.col-xs-12 > .row > .col-xs-12 > .fl-btn > .fl-btn--inner');
+  await page.click('.col-xs-12 > .row > .col-xs-12 > .fl-btn > .fl-btn--inner');
   
-  await page.waitForSelector('.ncss-col-sm-12 > .buying-tools-container > .size-layout > .size:nth-child(7) > .size-grid-dropdown')
-  await page.click('.ncss-col-sm-12 > .buying-tools-container > .size-layout > .size:nth-child(7) > .size-grid-dropdown')
+  await page.waitForSelector('.fl-minicart--productlist > .fl-minicart--checkout > .fl-minicart--checkout__checkout > .fl-btn > .fl-btn--inner');
+  await page.click('.fl-minicart--productlist > .fl-minicart--checkout > .fl-minicart--checkout__checkout > .fl-btn > .fl-btn--inner');
   
-  await page.waitForSelector('.ncss-col-sm-12 > .ncss-col-sm-12 > .buying-tools-container > .mt2-sm > .ncss-brand')
-  await page.click('.ncss-col-sm-12 > .ncss-col-sm-12 > .buying-tools-container > .mt2-sm > .ncss-brand')
+  // await navigationPromise
   
-  await page.waitForSelector('.d-md-t > .d-md-tc > .cart-item-modal-content-container > .ncss-row > .ncss-btn-black')
-  await page.click('.d-md-t > .d-md-tc > .cart-item-modal-content-container > .ncss-row > .ncss-btn-black')
-  
-  await navigationPromise
-  
-  await browser.close()
+  await browser.close();
 })()
