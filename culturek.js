aee3a36c3c48224de475a54a79819f1d5af90ca0
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-01-28 10:50:18 +0800

    try catch nest

--- culturek.js
+++ culturek.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer")
 
 // function chill() {
 //   console.log("initiating wait")
@@ -20,59 +20,61 @@ const puppeteer = require('puppeteer');
 //   console.timeEnd('delay');
 // })();
 
+;(async () => {
+	try {
+		const browser = await puppeteer.launch({ ignoredHTTPSErrors: true, headless: false })
+		const page = await browser.newPage()
 
-(async () => {
+		const navigationPromise = page.waitForNavigation()
 
-  const browser = await puppeteer.launch({ignoredHTTPSErrors: true, headless: false})
-  const page = await browser.newPage()
-  
-  const navigationPromise = page.waitForNavigation()
-  
-  await page.goto('https://www.culturekings.com.au/products/jordan-jordan-air-jordan-1-retro-high-og-black-silver-red')
-  
-  await page.setViewport({ width: 1279, height: 618 })
-  
-  await page.waitForSelector('.form-horizontal > .product-single__options > .selector-container > .variant-selector > .radio-button:nth-child(13)')
-  await page.click('.form-horizontal > .product-single__options > .selector-container > .variant-selector > .radio-button:nth-child(13)')
-  
-  await page.waitForSelector('.form-horizontal > .add-to-cart-button > .btn > .button-content > .btn__content')
-  await page.click('.form-horizontal > .add-to-cart-button > .btn > .button-content > .btn__content')
-  
-  await page.waitFor(4000);
+		await page.goto("https://www.culturekings.com.au/products/jordan-jordan-air-jordan-1-retro-high-og-black-silver-red")
 
-  // chill();
+		await page.setViewport({ width: 1279, height: 618 })
 
-// await delay(4000)
+		await page.waitForSelector(".form-horizontal > .product-single__options > .selector-container > .variant-selector > .radio-button:nth-child(13)")
+		await page.click(".form-horizontal > .product-single__options > .selector-container > .variant-selector > .radio-button:nth-child(13)")
 
-  await page.waitForSelector('#shopify-section-header > #CartDrawer > #CartContainer > .ajaxcart__footer > .btn--primary')
-  await page.click('#shopify-section-header > #CartDrawer > #CartContainer > .ajaxcart__footer > .btn--primary')
-  
-  await navigationPromise
-  
-  await page.waitForSelector('.fieldset-description #checkout_buyer_accepts_marketing')
-  await page.click('.fieldset-description #checkout_buyer_accepts_marketing')
-  
-  await page.waitForSelector('.section__content #checkout_email')
-  await page.click('.section__content #checkout_email')
-  
-  await page.type('.section__content #checkout_email', 'test_email@yahoo.com')
-  
-//   await page.type('.section__content #checkout_email', 'aj_funelas@yahoo.com')
-  
-  await page.type('.fieldset-description #checkout_buyer_accepts_marketing', '1')
-  
-  await page.type('.section__content #checkout_shipping_address_first_name', 'Aj')
-  
-  await page.waitForSelector('.section__content #checkout_shipping_address_address1')
-  await page.click('.section__content #checkout_shipping_address_address1')
-  
-  await page.waitForSelector('.edit_checkout > .step__sections > .section--shipping-address > .section__content > .fieldset')
-  await page.click('.edit_checkout > .step__sections > .section--shipping-address > .section__content > .fieldset')
-  
-  await page.select('.section__content #checkout_shipping_address_province', 'WA')
-  
-  await page.waitForSelector('.section__content #checkout_shipping_address_phone')
-  await page.click('.section__content #checkout_shipping_address_phone')
-  
-//   await browser.close()
+		await page.waitForSelector(".form-horizontal > .add-to-cart-button > .btn > .button-content > .btn__content")
+		await page.click(".form-horizontal > .add-to-cart-button > .btn > .button-content > .btn__content")
+
+		await page.waitFor(4000)
+
+		// chill();
+
+		// await delay(4000)
+
+		await page.waitForSelector("#shopify-section-header > #CartDrawer > #CartContainer > .ajaxcart__footer > .btn--primary")
+		await page.click("#shopify-section-header > #CartDrawer > #CartContainer > .ajaxcart__footer > .btn--primary")
+
+		await navigationPromise
+
+		await page.waitForSelector(".fieldset-description #checkout_buyer_accepts_marketing")
+		await page.click(".fieldset-description #checkout_buyer_accepts_marketing")
+
+		await page.waitForSelector(".section__content #checkout_email")
+		await page.click(".section__content #checkout_email")
+
+		await page.type(".section__content #checkout_email", "test_email@yahoo.com")
+
+		//   await page.type('.section__content #checkout_email', 'aj_funelas@yahoo.com')
+
+		await page.type(".fieldset-description #checkout_buyer_accepts_marketing", "1")
+
+		await page.type(".section__content #checkout_shipping_address_first_name", "Aj")
+
+		await page.waitForSelector(".section__content #checkout_shipping_address_address1")
+		await page.click(".section__content #checkout_shipping_address_address1")
+
+		await page.waitForSelector(".edit_checkout > .step__sections > .section--shipping-address > .section__content > .fieldset")
+		await page.click(".edit_checkout > .step__sections > .section--shipping-address > .section__content > .fieldset")
+
+		await page.select(".section__content #checkout_shipping_address_province", "WA")
+
+		await page.waitForSelector(".section__content #checkout_shipping_address_phone")
+		await page.click(".section__content #checkout_shipping_address_phone")
+	} catch (e) {
+		console.log("Error:" + e.message)
+	}
+
+	//   await browser.close()
 })()
